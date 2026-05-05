439f8f02b321264d0bbf64d4b901f8198a9a36d1
mgcoach <vagalsete@gmail.com>
2026-05-05 14:00:47 -0300

    v1.4 - correção de GIFs errados

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// TRAIN.FUEL - Service Worker v1.3.0
-const CACHE_NAME = 'trainfuel-v1-3-0';
+// TRAIN.FUEL - Service Worker v1.4.0
+const CACHE_NAME = 'trainfuel-v1-4-0';
 const ASSETS = [
   './',
   './index.html',
