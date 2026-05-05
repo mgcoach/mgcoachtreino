1c9c14446242b44669adad35e4d5adba36972fd5
mgcoach <vagalsete@gmail.com>
2026-05-05 09:39:54 -0300

    Atualização v1.2 - PDF com GIFs e fix Chrome

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// TRAIN.FUEL - Service Worker v1.0.0
-const CACHE_NAME = 'trainfuel-v1-0-0';
+// TRAIN.FUEL - Service Worker v1.2.0
+const CACHE_NAME = 'trainfuel-v1-2-0';
 const ASSETS = [
   './',
   './index.html',
