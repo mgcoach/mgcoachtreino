95c95b96939e0434a08f921f822370f343245dc8
mgcoach <vagalsete@gmail.com>
2026-05-06 13:43:56 -0300

    v2.3 - remove duplicatas que escondiam GIFs

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// TRAIN.FUEL - Service Worker v2.1.0
-const CACHE_NAME = 'trainfuel-v2-1-0';
+// TRAIN.FUEL - Service Worker v2.3.0
+const CACHE_NAME = 'trainfuel-v2-3-0';
 const ASSETS = [
   './',
   './index.html',
